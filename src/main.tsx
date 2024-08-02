11275143081580bd71fc2e4eae9b4dda960e4f85
Luciana <luciana.holzmann@gmail.com>
2024-08-02 02:21:49 +0200

    [STYLE] Archivos generales, mixins, fuente

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import TodoList from './pages/TodoList';
-import './styles/Home.scss';
+import './styles/index';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/styles/index.ts
@@ -0,0 +1,8 @@
+
+export * from './style.todoList.scss';
+export * from './style.todoButton.scss';
+export * from './style.todoCounter.scss';
+export * from './style.todoForm.scss';
+export * from './style.todoItems.scss';
+export * from './style.todoSearch.scss';
+export * from './mixins.scss';
